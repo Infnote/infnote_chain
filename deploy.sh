523d96a21110bff823da5f814e786d1276b74ae9
Vergil Choi <gameboy0824@126.com>
2018-10-23 14:50:14 +0800

    Fix wrong script run

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ export PATH=/usr/local/bin:$PATH
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 pipenv install
-pyinstaller main.spec -y
+pipenv run pyinstaller main.spec -y
 cp ./dist/main /usr/local/bin/ichain
 ichain server restart
