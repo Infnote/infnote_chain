c414d8d15332baeb9b7aab36eef42d2301e8cbed
Vergil Choi <gameboy0824@126.com>
2018-10-23 16:29:55 +0800

    Add missing dependencies

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
-pwd
+
+# install python environment in project path
+export PIPENV_VENV_IN_PROJECT=1
+
+# add bin path
 export PATH=/usr/local/bin:$PATH
+
+# avoid python3 setting problem
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
+
+# install packages
 pipenv install
+# generate executable file
 pipenv run pyinstaller ./main.spec -y
-cp ./dist/main /usr/local/bin/ichain
+# copy to global
+cp -f ./dist/main /usr/local/bin/ichain
+# start server
 ichain server restart
