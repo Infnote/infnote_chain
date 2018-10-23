74dee1adb9836f483c18358eba2e63adb87a8767
Vergil Choi <gameboy0824@126.com>
2018-10-23 14:57:16 +0800

    Set current path

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
+pwd
 export PATH=/usr/local/bin:$PATH
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 pipenv install
-pipenv run pyinstaller main.spec -y
+pipenv run pyinstaller ./main.spec -y
 cp ./dist/main /usr/local/bin/ichain
 ichain server restart
